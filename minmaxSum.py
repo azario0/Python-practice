908ec92570e108402bafd01ffb8306bd6df5e8ce
Mfundo-debug <diditmfundo@gmail.com>
2023-03-31 05:55:50 +0200

    find minmax in arr

---
+++ minmaxSum.py
@@ -0,0 +1,9 @@
+def miniMaxSum(arr):
+    arr.sort()
+    min_sum = sum(arr[:-1])
+    max_sum = sum(arr[1:])
+    print(min_sum, max_sum)
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+    miniMaxSum(arr)
