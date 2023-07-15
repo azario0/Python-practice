2769a712776d8856740498be51f38754bad0d4ac
Mfundo_debug <diditmfundo@gmail.com>
2023-07-15 18:32:21 +0200

    documented

--- findstring.py
+++ findstring.py
@@ -4,15 +4,23 @@ In this challenge, the user enters a string and a substring. You have to print t
 NOTE: String letters are case-sensitive.
 """
 def count_substring(string, sub_string):
+    #initialize a count for the number of times the substring appears
     count = 0
+    #loop through the string, starting at each index
     for i in range(len(string)):
+        #if the substring is found at this index, add 1 to the count
         if string[i:].startswith(sub_string):
             count+=1
+    #return the final count
     return count
 
 if __name__ == '__main__':
+    #get the string from the user
     string = input().strip()
+    #get the substring from the user
     sub_string = input().strip()
     
+    #call the function and store the result
     count = count_substring(string, sub_string)
+    #print the result
     print(count)
