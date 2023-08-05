8319327a98c6710d2b363bd97c5f845a33b69a68
Mfundo-debug <diditmfundo@gmail.com>
2023-08-05 02:50:32 +0200

    calendar module

---
+++ cal_mod.py
@@ -0,0 +1,24 @@
+"""
+Task
+
+You are given a date. Your task is to find what the day is on that date.
+
+Input Format
+
+A single line of input containing the space separated month, day and year, 
+respectively, in MMDDYYYY format.
+
+Constraints
+ 2000 < year < 3000
+"""
+import calendar
+
+def main():
+    date = input().split()
+    month = int(date[0])
+    day = int(date[1])
+    year = int(date[2])
+    print(calendar.day_name[calendar.weekday(year, month, day)].upper())
+    
+if __name__ == '__main__':
+    main()
