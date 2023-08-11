d2566043e1fbb32763b555e0035bb8e4d505b79e
Mfundo-debug <diditmfundo@gmail.com>
2023-08-11 15:30:49 +0200

    10/11 test cases failed

--- lattice_path.py
+++ lattice_path.py
@@ -4,16 +4,18 @@ You can start by initializing a 2D array with all values set to 1.
 Then, iterate through the array and update each value by adding the value above and to the left of it. 
 Finally, return the value at the bottom right corner of the array.
 """
-def lattice_paths(n):
-    # Initialize a 2D array of 1's
-    paths = [[1 for _ in range(n + 1)] for _ in range(n + 1)]
+def lattice_paths(n, m):
+    # Initialize a 2D array with all values of 1's
+    paths = [[1] * (m+1) for _ in range(n+1)]
+    #update each value using dynamic programming
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            paths[i][j] = paths[i-1][j] + paths[i][j-1]
+            #return the value at the bottom right corner of the array
+    return paths[n][m]
 
-    # Calculate the number of paths for each point on the grid
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
-
-    # Return the value at the bottom right corner of the array
-    return paths[n][n]
-if __name__ == "__main__":
-    print(lattice_paths(int(input("Enter the number: "))))
+# Get input from user
+t = int(input("Enter the number of test cases: "))
+for i in range(t):
+    m, n = map(int, input("Enter the dimensions of the grid: ").split())
+    print(lattice_paths(m, n))
