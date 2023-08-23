1e19db0879aef299f4a14ccc65de5f1b8f8e2f68
Mfundo-debug <diditmfundo@gmail.com>
2023-08-23 06:35:48 +0200

    trash bing

--- num_to_words.py
+++ num_to_words.py
@@ -2,7 +2,9 @@
 The numbers 1 to 5 written out in words are One, Two, Three, Four, Five
 
 First character of each word will be capital letter for example:
- 10438242112 is Given a number, you have to write it in words.
+ 10438242112 is One Hundred Four Billion Three Hundred Eighty Two Million Four Hundred Twenty One Thousand One Hundred Twelve
+ 
+ Given a number, you have to write it in words.
 
 Input Format
 
@@ -13,23 +15,63 @@ Constraints
 1 <= T <= 10
 0 <= N <= 10^12
 """
-def num_to_words(N,T,words):
-    for i in range(T):
-        if N[i] == 0:
-            print("Zero")
-        else:
-            print(words[N[i]])
-if __name__ == "__main__":
-    T = int(input())
-    N = []
-    words = {0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",
-             7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Eleven",12:"Twelve",
-             13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",
-             17:"Seventeen",18:"Eighteen",19:"Nineteen",20:"Twenty",
-             30:"Thirty",40:"Forty",50:"Fifty",60:"Sixty",70:"Seventy",
-             80:"Eighty",90:"Ninety",100:"Hundred",1000:"Thousand",
-             100000:"Lakh",10000000:"Crore"}
-    for i in range(T):
-        N.append(int(input()))
-    num_to_words(N,T,words)
+# Define a dictionary of words representing the numbers 1 to 10
+words = {i: word for i, word in enumerate(["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"])}
+
+# Define a dictionary of words representing the numbers 11 to 19
+teens = {i: word for i, word in enumerate(["Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"], start=11)}
+
+# Define a dictionary of words representing the tens place of numbers
+tens = {i: word for i, word in enumerate(["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"])}
+
+# Define a list of words representing the thousands, millions, billions, and trillions place of numbers
+suffixes = ["", "Thousand", "Million", "Billion", "Trillion"]
+
+def num_to_words(n):
+    # If the input number is 0, return "Zero"
+    if n == 0:
+        return "Zero"
     
+    # Convert the input number to a string and split it into groups of three digits
+    num_str = str(n)
+    groups = []
+    while num_str:
+        groups.append(num_str[-3:])
+        num_str = num_str[:-3]
+    
+    # Convert each group of three digits into its corresponding word representation
+    words_list = []
+    for i, group in enumerate(groups):
+        group_words = []
+        group = int(group)
+        if group >= 100:
+            group_words.append(words[group // 100] + " Hundred")
+            group %= 100
+        if group >= 11 and group <= 19:
+            group_words.append(teens[group])
+        elif group == 10 or group >= 20:
+            if group % 10 != 0:
+                group_words.append(tens[group // 10] + " " + words[group % 10])
+            else:
+                group_words.append(tens[group // 10])
+        elif group >= 1 and group <= 9:
+            group_words.append(words[group])
+        if group != 0:
+            group_words.append(suffixes[i])
+        words_list.append(" ".join(group_words))
+    
+    # Reverse the list of word representations and join them together with spaces
+    words_list.reverse()
+    return " ".join(words_list)
+
+if __name__ == "__main__":
+    # Read the number of test cases from standard input
+    t = int(input())
+    
+    # Loop through each test case
+    for i in range(t):
+        # Read the input number from standard input
+        n = int(input())
+        
+        # Convert the input number to its corresponding word representation and write it to standard output
+        print(num_to_words(n))
