a9185997993a5b2ea9dc11af8728929d40a5e15d
azario0 <azariomalek@gmail.com>
2023-08-26 04:59:02 +0100

    Made the code more readable

--- arr.py
+++ arr.py
@@ -17,13 +17,14 @@ def plusMinus(arr):
     positive = sum(x > 0 for x in arr)
     negative = sum(x < 0 for x in arr)
     zero = sum(x == 0 for x in arr)
-    print("{:.6f}".format(positive/n))
-    print("{:.6f}".format(negative/n))
-    print("{:.6f}".format(zero/n))
+    print("Percentage of positive integers : {:.6f}".format(positive/n))
+    print("Percentage of negative integers : {:.6f}".format(negative/n))
+    print("Percentage of zero integers : {:.6f}".format(zero/n))
             
 if __name__ == '__main__':
-    n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+    arr = list(map(int, input('Enter the integers separated by a coma : ').rstrip().split(',')))
 
     plusMinus(arr)
+
+
